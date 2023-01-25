59f21ab2b0f864e44fcc2e675beca8dc0382a6dc
Adrian-Garcia <adriangarlop@hotmail.com>
2023-01-25 17:14:57 -0600

    made_base_layout to work

--- financial_analyst/stock_report/urls.py
+++ financial_analyst/stock_report/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 app_name = "stock_reports"
 urlpatterns = [
+    # ex: /stock_report/
     path("", views.IndexView.as_view(), name="index"),
+    
+    # ex: /stock_report/1/
     path("<int:fundamental_analysis_id>/", views.detail, name="detail"),
+    
+    # ex: /stock_report/new/
     path("new/", views.new_fundamental_analysis, name="new"),
+    
+    # ex: POST /stock_report/create/
     path("create/", views.create_fundamental_analysis, name="create"),
 ]
