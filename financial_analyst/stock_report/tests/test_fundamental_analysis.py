aed2a6a4b5995309e8ba0315c58a567e98e8360a
Adrian-Garcia <adriangarlop@hotmail.com>
2023-01-26 13:26:14 -0600

    Added tests for fundamental analysis

---
+++ financial_analyst/stock_report/tests/test_fundamental_analysis.py
@@ -0,0 +1,41 @@
+from typing import List
+from stock_report.tests.test_stock import create_stock
+from django.test import TestCase
+from django.urls import reverse
+from stock_report.models import FundamentalAnalysis, Stock
+
+
+def create_fundamental_analysis(name: str) -> FundamentalAnalysis:
+    return FundamentalAnalysis.objects.create(name=name)
+
+
+class FundamentalAnalysisDetailViewTest(TestCase):
+    def test_existing_fundamental_analysis_with_stocks(self) -> None:
+        fundamental_analysis = create_fundamental_analysis("FANG")
+        google_stock = create_stock("Google", "GOOGL", [fundamental_analysis])
+        amazon_stock = create_stock("Amazon", "AMZN", [fundamental_analysis])
+
+        response = self.client.get(
+            reverse("stock_reports:detail", args=(fundamental_analysis.id,))
+        )
+
+        self.assertContains(response, fundamental_analysis)
+        self.assertContains(response, google_stock.name)
+        self.assertContains(response, amazon_stock.name)
+
+    def test_existing_fundamental_analysis_without_stocks(self) -> None:
+        fundamental_analysis = create_fundamental_analysis("FANG")
+
+        response = self.client.get(
+            reverse("stock_reports:detail", args=(fundamental_analysis.id,))
+        )
+
+        self.assertContains(response, fundamental_analysis)
+        self.assertContains(
+            response, "El análisis fundamental no tiene acciones para analizar"
+        )
+
+    def test_nonexistent_fundamental_analysis(self) -> None:
+        response = self.client.get(reverse("stock_reports:detail", args=(0,)))
+
+        self.assertEqual(response.status_code, 404)
--- financial_analyst/stock_report/tests/test_stock.py
+++ financial_analyst/stock_report/tests/test_stock.py
@@ -1,22 +1,28 @@
 from django.test import TestCase
 from django.urls import reverse
-from stock_report.models import Stock
+from stock_report.models import Stock, FundamentalAnalysis
 
 
-def createStock(name: str, ticker: str) -> Stock:
-    return Stock.objects.create(name=name, ticker=ticker)
+def create_stock(
+    name: str, ticker: str, fundamental_analyses: FundamentalAnalysis = None
+) -> Stock:
+    stock = Stock.objects.create(name=name, ticker=ticker)
+
+    if fundamental_analyses:
+        stock.fundamental_analyses.set(fundamental_analyses)
+
+    return stock
 
 
 class StockDetailViewTest(TestCase):
-    def test_nonexistent_stock(self):
+    def test_nonexistent_stock(self) -> None:
         response = self.client.get(reverse("stock_reports:stock_detail", args=(0,)))
         self.assertEqual(response.status_code, 404)
 
-    def test_existing_stock(self):
-        stock = createStock("Google", "GOOGL")
+    def test_existing_stock(self) -> None:
+        stock = create_stock("Google", "GOOGL")
         response = self.client.get(
             reverse("stock_reports:stock_detail", args=(stock.id,))
         )
 
-        self.assertContains(response, stock.name)
-        self.assertContains(response, stock.ticker)
+        self.assertContains(response, stock)
