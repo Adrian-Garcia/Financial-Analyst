81019c48753cfdeb291bf1c97da090e88e0406cd
Adrian-Garcia <adriangarlop@hotmail.com>
2023-01-30 17:52:01 -0600

    Deleted duplicated code

--- financial_analyst/stock_report/error_handler/fundamental_analysis_error_handler.py
+++ financial_analyst/stock_report/error_handler/fundamental_analysis_error_handler.py
@@ -1,6 +1,6 @@
 from typing import List
+from stock_report.error_handler.stock_error_handler import not_found_tickers
 from stock_report.models.fundamental_analysis_model import FundamentalAnalysis
-from stock_report.models.stock_model import Stock
 
 
 def get_fundamental_analysis_errors(
@@ -10,8 +10,5 @@ def get_fundamental_analysis_errors(
     if not fundamental_analysis.name:
         errors.append("Analisis Fundamental debe tener nombre")
 
-    for stock in stocks:
-        if type(stock) != Stock:
-            errors.append(f"El ticker {stock} no pudo ser encontrado")
-
+    errors += not_found_tickers(stocks)
     return errors
--- financial_analyst/stock_report/error_handler/stock_error_handler.py
+++ financial_analyst/stock_report/error_handler/stock_error_handler.py
@@ -3,15 +3,23 @@ from stock_report.models.fundamental_analysis_model import FundamentalAnalysis
 from stock_report.models.stock_model import Stock
 
 
-def get_stock_errors(
-    fundamental_analysis: FundamentalAnalysis, stocks: List[str]
-) -> List[str]:
+def not_found_tickers(stocks: List[str]) -> List[str]:
     errors = []
+
     for stock in stocks:
         if type(stock) != Stock:
             errors.append(f"El ticker {stock} no pudo ser encontrado")
 
-        elif stock in fundamental_analysis.stock_set.all():
+    return errors
+
+
+def get_stock_errors(
+    fundamental_analysis: FundamentalAnalysis, stocks: List[str]
+) -> List[str]:
+    errors = not_found_tickers(stocks)
+
+    for stock in stocks:
+        if stock in fundamental_analysis.stock_set.all():
             errors.append(f"{stock} ya se encuentra dentro del analisis fundamental")
 
     return errors
