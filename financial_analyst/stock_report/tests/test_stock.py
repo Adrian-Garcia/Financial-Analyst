1de3c7434be6f929542fcd1b79d0cc073c5de1fb
Adrian-Garcia <adriangarlop@hotmail.com>
2023-02-13 20:05:48 -0600

    populated stock data for tests

--- financial_analyst/stock_report/tests/test_stock.py
+++ financial_analyst/stock_report/tests/test_stock.py
@@ -4,13 +4,33 @@ from stock_report.models.stock_model import Stock
 from stock_report.models.fundamental_analysis_model import FundamentalAnalysis
 import random
 
-# TODO: Implement Faker dependency on project
-
 
 def create_stock(
     ticker: str, name: str = None, fundamental_analyses: FundamentalAnalysis = None
 ) -> Stock:
-    stock = Stock.objects.create(name=name, ticker=ticker, price=random.uniform(1, 100))
+    # TODO: Implement a way to populate stock data more accurately
+    random_price = random.uniform(1, 100)
+
+    stock = Stock.objects.create(
+        name=name,
+        ticker=ticker,
+        price=random_price,
+        price_bid=random_price * 0.98,
+        price_ask=random_price * 1.02,
+        current_ratio=random.uniform(1, 3),
+        quick_ratio=random.uniform(1, 3),
+        cash_ratio=random.uniform(1, 3),
+        debt_equity=random.uniform(1, 3),
+        inventory_turnover=random.uniform(0, 150),
+        days_inventory=random.uniform(0, 50),
+        assets_turnover=random.uniform(0, 2),
+        return_on_equity=random.uniform(0, 2),
+        net_margin=random.uniform(0, 1),
+        price_earnings=random.uniform(10, 100),
+        price_cash_flow=random.uniform(10, 100),
+        price_to_sales=random.uniform(0, 20),
+        price_to_book=random.uniform(0, 20),
+    )
 
     if fundamental_analyses:
         stock.fundamental_analyses.set(fundamental_analyses)
